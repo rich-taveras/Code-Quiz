64b6bb4b82b91505e29cbc3a518c5f1f97f6d12a
rich-taveras <rtaveras@yahoo.com>
2023-07-06 17:41:23 -0400

    Added files

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var questions = [
+    {
+        question: "Question 1", 
+        answers: [
+            { text: "answer 1", correct: false},
+            { text: "answer 1", correct: true},
+            { text: "answer 1", correct: false},
+            { text: "answer 1", correct: false},
+        ]    },
+];
+var questionElement = document.getElementById("question");
