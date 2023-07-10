8113f9b0df38d7315951a205045dca29bfd4d1b1
rich-taveras <rtaveras@yahoo.com>
2023-07-09 20:17:58 -0400

    added timer

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
+function setTime() {
+  // Sets interval in variable
+  var timerInterval = setInterval(function() {
+    secondsLeft--;
+    timeEl.textContent = secondsLeft + " seconds left ";
+
+    if(secondsLeft === 0) {
+      // Stops execution of action at set interval
+      clearInterval(timerInterval);
+      // Calls function to create and append image
+      sendMessage();
+    }
+
+  }, 1000);
+}
+
+setTime();
+
+
+
 const questions = [
   {
     question: "Question 1",
@@ -10,6 +30,24 @@ const questions = [
   },
   {
     question: "Question 2",
+    answers: [
+      { text: "answer 1", correct: false },
+      { text: "answer 2", correct: true },
+      { text: "answer 3", correct: false },
+      { text: "answer 4", correct: false },
+    ],
+  },
+  {
+    question: "Question 3",
+    answers: [
+      { text: "answer 1", correct: false },
+      { text: "answer 2", correct: true },
+      { text: "answer 3", correct: false },
+      { text: "answer 4", correct: false },
+    ],
+  },
+  {
+    question: "Question 4",
     answers: [
       { text: "answer 1", correct: false },
       { text: "answer 2", correct: true },
@@ -33,25 +71,79 @@ function startQuiz() {
 }
 
 function showQuestion() {
-    resetState();
-  let currentQuestionIndex = question[currentQuestionIndex];
+  resetState();
+  let currentQuestionIndex = questions[currentQuestionIndex];
   let questionNo = currentQuestionIndex + 1;
-  questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+  questionElement.innerHTML = questionNo + ". " + currentQuestionIndex.question;
 
-  currentQuestion.answers.forEach(answer => {
+  currentQuestionIndex.answers.forEach((answer) => {
     const button = document.createElement("button");
     button.innerHTML = answer.text;
     button.classList.add("btn");
-    answerButton.appendChild(button);
+    answerButtons.appendChild(button);
+    if (answer.correct) {
+      button.dataset.correct = answer.correct;
+    }
+    button.addEventListener("click", selectAnswer);
   });
 }
 
-function resetState(){
-    nextButton.style.display = "none";
-    while(answerButtons.firstchild){
-        andswerButtons.removeChild(answerButtons.firstchild);
+function resetState() {
+  nextButton.style.display = "none";
+  while (answerButton.firstchild) {
+    answerButton.removeChild(answerButton.firstchild);
+  }
+}
 
+function selectAnswer(e){
+  const selectedBtn = e.target;
+
+  const isCorrect = selectieBtn.dataset.correct === "true";
+  if(isCorrect){
+    selectedBtn.classList.add("correct");
+    score++;
+
+  }else{
+    selectedBtn.classList.add("incorrect");
+  }
+  Array.from(answerButtons.children).forEach(button => {
+    if(button.dataset.correct === "true"){
+      button.classList.add("correct");
     }
+    button.disable = true;
+  })
+  nextButton.style.display = "block";
+}
+
+function showScore(){
+  resetState();
+  questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
+  nextButton.innerHTML = "Test Again";
+  nextButton.style.display = "block";
+
+function handleNextButton(){
+  currentQuestionIndex++;
+  if(currentQuestionIndex < questions.length){
+    showQuestion();
+  }else{
+    showScore();
+  }
 
 }
+
+nextButton.addEventListener("click", ()=>{
+  if(currentQuestionIndex < questions.length){
+    handleNextButton();
+  }else{
+    startQuiz();
+  }
+})
 startQuiz();
+}
+
+
+
+var timeEl = document.querySelector("#timer");
+
+var secondsLeft = 60;
+
---
+++ js/timer.js
@@ -0,0 +1,39 @@
+// var secondsLeft = 50;
+
+// var timeEl = document.getElementById("timer");
+
+// timerCountdown();
+// function timerCountdown () {
+// var timerInterval = setInterval(function() {
+//     secondsLeft--;
+//     timeEl.textContent = secondsLeft + " seconds left ";
+
+//     if(secondsLeft === 0) {
+//       clearInterval(timerInterval);
+//     }
+
+//   }, 1000);
+// }
+
+// Selects element by class
+// var timeEl = document.querySelector("#timer");
+
+// var secondsLeft = 60;
+
+// function setTime() {
+//   // Sets interval in variable
+//   var timerInterval = setInterval(function() {
+//     secondsLeft--;
+//     timeEl.textContent = secondsLeft + " seconds left ";
+
+//     if(secondsLeft === 0) {
+//       // Stops execution of action at set interval
+//       clearInterval(timerInterval);
+//       // Calls function to create and append image
+//       sendMessage();
+//     }
+
+//   }, 1000);
+// }
+
+// setTime();
