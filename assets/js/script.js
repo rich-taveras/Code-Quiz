1425b2849edfd7c2dc7e9739dbea3cd954847af0
rich-taveras <rtaveras@yahoo.com>
2023-07-13 07:21:55 -0400

    added 5 questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 var startBtn = document.getElementById("start-btn");
 var introSecEl = document.getElementById("intro-section");
 
-var questionSection = document.getElementById("question-section");
+var questionSectionEl = document.getElementById("question-section");
 var titleEl = document.getElementById("title");
 var timerEl = document.getElementById("timer");
 var choicesEl = document.querySelectorAll(".choices");
@@ -10,17 +10,48 @@ var choicesEl = document.querySelectorAll(".choices");
 var questionIndex = 0;
 var questions = [
   {
-    question: "What is the capital of France?",
-    choices: ["Paris", "London", "Berlin", "Rome"],
-    answer: "Paris",
+    question: "What does HTML stand for?",
+    choices: [
+      "Hyper Text Markup Language",
+      "Home Tool Markup Language",
+      "Hyperlinks and Text Markup Language",
+      "Hyperlinking Text and Multimedia Language"
+    ],
+    answer: 0
   },
   {
-    question: "What is the capital of Puerto Rico?",
-    choices: ["Ponce", "Caguas", "San Juan", "Arecibo"],
-    answer: "San Juan",
+    question: "Which CSS property is used to change the font size?",
+    choices: ["font-size", "text-size", "font-style", "text-style"],
+    answer: 0
   },
-  // Add more questions as needed
+  {
+    question: "What is the correct JavaScript syntax to output 'Hello, World!' in the console?",
+    choices: [
+      "console.out('Hello, World!');",
+      "console.log('Hello, World!');",
+      "print('Hello, World!');",
+      "log('Hello, World!');"
+    ],
+    answer: 1
+  },
+  {
+    question: "Which HTML tag is used to link an external JavaScript file?",
+    choices: ["<js>", "<script>", "<javascript>", "<link>"],
+    answer: 1
+  },
+  {
+    question: "What is the purpose of CSS media queries?",
+    choices: [
+      "To create responsive web designs",
+      "To validate HTML syntax",
+      "To execute JavaScript code",
+      "To style HTML elements"
+    ],
+    answer: 0
+  }
 ];
+
+  // Add more questions as needed
 var timeLeft = questions.length * 15;
 
 /*
@@ -34,7 +65,7 @@ var setIntervalId = 0;
 function startQuiz() {
   introSecEl.style.display = "none";
   //  introSecEl.classList.add("hide")
-  questionSection.style.display = "block";
+  questionSectionEl.style.display = "block";
   setIntervalId = setInterval(countDown, 1000);
   showQuestions()
   // startTimer();
@@ -58,9 +89,17 @@ function showQuestions() {
 
 
 }
-startBtn.addEventListener("click", startQuiz);
-{
+
+function nextQuestion(event){
+  var currentElement= event.target
+  if(currentElement.matches("button")){
+    questionIndex++
+    showQuestions()
+  }
 }
+startBtn.addEventListener("click", startQuiz);
+
+questionSectionEl.addEventListener("click", nextQuestion);
 
 // // (STARTER CODE)
 
